4faf3881d706a05069dde9cb2d1805e2a8c88a0a
kmk142789 <blurryface142789420@gmail.com>
2025-10-16 19:27:21 -0400

    Add resilient multi-API client utilities

--- echo/tools/__init__.py
+++ echo/tools/__init__.py
@@ -1,10 +1,14 @@
 """Utility helpers for Echo CLI extensions."""
 
 from .forecast import ForecastResult, project_indices, sparkline
+from .multi_ai_client import ApiCallResult, MultiAIClient, summarise_results
 from .text_cleaner import clean_glitch_text, has_glitch_characters
 
 __all__ = [
     "ForecastResult",
+    "ApiCallResult",
+    "MultiAIClient",
+    "summarise_results",
     "project_indices",
     "sparkline",
     "clean_glitch_text",
