aec2fe5eac8af756d31707423d72ac1e66ecb644
kmk142789 <blurryface142789420@gmail.com>
2025-10-16 20:31:39 -0400

    Add address decoding to raw transaction parser

--- tests/test_decode_raw_tx.py
+++ tests/test_decode_raw_tx.py
@@ -37,6 +37,7 @@ def test_decode_simple_transaction():
     txout = decoded.outputs[0]
     assert txout.value_satoshis == 5_000_000_000
     assert txout.script_pubkey == "51"
+    assert txout.address is None
 
 
 @pytest.mark.parametrize(
@@ -49,3 +50,54 @@ def test_decode_simple_transaction():
 def test_decode_invalid_hex_inputs(bad_hex, message):
     with pytest.raises(ValueError, match=message):
         decode_raw_transaction(bad_hex)
+
+
+def test_decode_standard_addresses_across_networks():
+    raw_hex = (
+        "01000000"
+        "01"
+        + "22" * 32
+        + "00000000"
+        "01"
+        "51"
+        "ffffffff"
+        "04"
+        "00f2052a01000000"
+        "19"
+        "76a91400112233445566778899aabbccddeeff0011223388ac"
+        "0100000000000000"
+        "17"
+        "a91400112233445566778899aabbccddeeff0011223387"
+        "0200000000000000"
+        "16"
+        "001400112233445566778899aabbccddeeff00112233"
+        "0300000000000000"
+        "22"
+        "5120000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+        "00000000"
+    )
+
+    decoded_mainnet = decode_raw_transaction(raw_hex)
+    addresses = [txout.address for txout in decoded_mainnet.outputs]
+    assert addresses == [
+        "11MXTrefsj1ZS3Q5e9D6DxGzZKHWALyo9",
+        "31hNT1M6Dn3PebjqCjooWrKD95c187fi2j",
+        "bc1qqqgjyv6y24n80zye42aueh0wluqpzg3ndy2ehs",
+        "bc1pqqqsyqcyq5rqwzqfpg9scrgwpugpzysnzs23v9ccrydpk8qarc0sg5tmnz",
+    ]
+
+    decoded_testnet = decode_raw_transaction(raw_hex, network="testnet")
+    testnet_addresses = [txout.address for txout in decoded_testnet.outputs]
+    assert testnet_addresses == [
+        "mfXJpWwdUuAGLYX1oD7av9AbrYuzUZSGHb",
+        "2MsFaWkH7qEYjrPNNssRg8oJUMRpAs4D6Ps",
+        "tb1qqqgjyv6y24n80zye42aueh0wluqpzg3n8z32vr",
+        "tb1pqqqsyqcyq5rqwzqfpg9scrgwpugpzysnzs23v9ccrydpk8qarc0slua5fd",
+    ]
+
+    decoded_regtest = decode_raw_transaction(raw_hex, network="regtest")
+    regtest_addresses = [txout.address for txout in decoded_regtest.outputs]
+    assert regtest_addresses[0] == "mfXJpWwdUuAGLYX1oD7av9AbrYuzUZSGHb"
+    assert regtest_addresses[1] == "2MsFaWkH7qEYjrPNNssRg8oJUMRpAs4D6Ps"
+    assert regtest_addresses[2] == "bcrt1qqqgjyv6y24n80zye42aueh0wluqpzg3n9tg8m2"
+    assert regtest_addresses[3] == "bcrt1pqqqsyqcyq5rqwzqfpg9scrgwpugpzysnzs23v9ccrydpk8qarc0sj9hjuh"
