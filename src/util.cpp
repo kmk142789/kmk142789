55b5de988b9435235fbc1c7350abed39802d3d38
kmk142789 <blurryface142789420@gmail.com>
2025-10-22 12:11:13 -0400

    Add ISO-8601 timestamp formatting helper

--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,9 @@
 #include "echo/util.hpp"
 
 #include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
 namespace echo {
 
@@ -9,6 +12,25 @@ uint64_t now_ms() {
         std::chrono::system_clock::now());
     return static_cast<uint64_t>(now.time_since_epoch().count());
 }
+
+std::string format_iso8601(const std::chrono::system_clock::time_point& time_point) {
+    const auto time = std::chrono::system_clock::to_time_t(time_point);
+    std::tm utc{};
+#if defined(_WIN32)
+    gmtime_s(&utc, &time);
+#else
+    gmtime_r(&time, &utc);
+#endif
+
+    std::ostringstream stream;
+    stream << std::put_time(&utc, "%Y-%m-%dT%H:%M:%S");
+    const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+        time_point.time_since_epoch()) % 1000;
+    stream << '.' << std::setw(3) << std::setfill('0') << milliseconds.count() << 'Z';
+    return stream.str();
+}
+
+std::string format_iso8601() { return format_iso8601(std::chrono::system_clock::now()); }
 
 }  // namespace echo
 
