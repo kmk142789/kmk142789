9e54bbda33fb00b3eea89bfe5ed9c933b5751bd8
kmk142789 <blurryface142789420@gmail.com>
2025-10-25 15:12:01 -0400

    Add Etherscan native balance client

--- packages/core/src/echo/tools/__init__.py
+++ packages/core/src/echo/tools/__init__.py
@@ -1,10 +1,18 @@
 """Utility helpers for Echo CLI extensions."""
 
+from .etherscan_balance import (
+    EtherscanAPIError,
+    EtherscanBalanceClient,
+    NativeBalance,
+)
 from .forecast import ForecastResult, project_indices, sparkline
 from .multi_ai_client import ApiCallResult, MultiAIClient, summarise_results
 from .text_cleaner import clean_glitch_text, has_glitch_characters
 
 __all__ = [
+    "EtherscanAPIError",
+    "EtherscanBalanceClient",
+    "NativeBalance",
     "ForecastResult",
     "ApiCallResult",
     "MultiAIClient",
