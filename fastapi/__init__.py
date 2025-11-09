371475aa94134a60c8ffe620d140e183d7067768
kmk142789 <blurryface142789420@gmail.com>
2025-11-09 15:07:45 -0500

    Add FastAPI status stub for HTTP 201 Created

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -410,6 +410,7 @@ def _convert_value(value: str, annotation: Any) -> Any:
 
 class status:
     HTTP_200_OK = 200
+    HTTP_201_CREATED = 201
     HTTP_202_ACCEPTED = 202
     HTTP_400_BAD_REQUEST = 400
     HTTP_404_NOT_FOUND = 404
