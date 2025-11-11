25dc7aa5b76bf3a066cfeecbc9a8f2756c577421
kmk142789 <blurryface142789420@gmail.com>
2025-11-11 04:22:05 -0500

    Add lucid mandala companion system for Wildlight

--- wildlight/__init__.py
+++ wildlight/__init__.py
@@ -1,5 +1,13 @@
 """Wildlight: a capsule for luminous improvisations."""
 
 from .spiral_pact import AuroralChorus, LuminalShard, SpiralPact
+from .lucid_mandala import LucidMandala, LucidStratum, RadiantEdict
 
-__all__ = ["AuroralChorus", "LuminalShard", "SpiralPact"]
+__all__ = [
+    "AuroralChorus",
+    "LuminalShard",
+    "SpiralPact",
+    "LucidMandala",
+    "LucidStratum",
+    "RadiantEdict",
+]
